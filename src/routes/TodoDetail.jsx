605272597694485cee86baa81bab7e02be484f88
SHIM-JINSEO <sjs011828@gmail.com>
2024-08-21 00:12:45 +0900

    fix: change TodoDetail page and delete unnecessary jsx file

--- src/routes/TodoDetail.jsx
+++ src/routes/TodoDetail.jsx
@@ -1,5 +1,18 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import '../App.css';
+
+function Checkbox({element}){
+    const [checked, setChecked] = useState(element.completed);
+  
+    return <input 
+              type='checkbox'
+              checked={checked}
+              onChange={(event)=>{
+                setChecked(!checked);
+              }}
+    />
+  }
 
 export default function TodoDetail(){
     const params = useParams();
@@ -9,11 +22,19 @@ export default function TodoDetail(){
           .then(response => response.json())
           .then(json => setTodo(json));
       }, [params.id]);
-    return  todo?<ul>
-        <li>{(todo.completed.toString())}</li>
-        <li>{todo.title}</li>
-        <li>{todo.userId}</li>
-    </ul>:null;
+    return  todo?<>
+    <div className='grid'>
+        <div className='completed'>COMPLETED</div>
+        <div className='title'>TODO</div>
+        <div className='userId'>USERID</div>
+        
+    </div>
+    <div className="grid" key={todo.id}>
+        <div className='completed'><Checkbox element={todo}/></div>
+        <div className='title'>{todo.title}</div>
+        <div className='userId'>{todo.userId}</div>
+    </div>
+      </>:null;
 }
 
 
