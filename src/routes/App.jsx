4bb185e93375ebd5fdf47148065cc3d8f6071835
SHIM-JINSEO <sjs011828@gmail.com>
2024-08-21 21:01:49 +0900

    feat: use router on search funtion

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,59 +1,76 @@
+import { useEffect, useMemo, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import "../App.css";
+import Search from "./Search.jsx";
 
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom'
-import '../App.css';
-import Search from './Search.jsx';
-
-function Checkbox({element}){
+function Checkbox({ element }) {
   const [checked, setChecked] = useState(element.completed);
 
-  return <input 
-            type='checkbox'
-            checked={checked}
-            onChange={(event)=>{
-              setChecked(!checked);
-            }}
-  />
+  return (
+    <input
+      type="checkbox"
+      checked={checked}
+      onChange={(event) => {
+        setChecked(!checked);
+      }}
+    />
+  );
 }
 
-
-function TodoTable({todos}){
-  
-  return <>
-    {todos.map(element=>(
-      <div className="grid" key={element.id}>
-        <div className='completed'><Checkbox element={element}/></div>
-        <div className='title'><Link to={`/${element.id}`}>{element.title}</Link></div>
-        <div className='userId'>{element.userId}</div>
-        
-      </div>
-    ))}
-  </>
+function TodoTable({ todos }) {
+  return (
+    <>
+      {todos.map((element) => (
+        <div className="grid" key={element.id}>
+          <div className="completed">
+            <Checkbox element={element} />
+          </div>
+          <div className="title">
+            <Link to={`/${element.id}`}>{element.title}</Link>
+          </div>
+          <div className="userId">{element.userId}</div>
+        </div>
+      ))}
+    </>
+  );
 }
 
 function App() {
-const [todos, setTodos] = useState([]);
-const [currentTodos, setCurrentTodos] = useState([]);
-
-  useEffect(()=>{
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => {
-        setTodos(json)
-        setCurrentTodos(json)
+  const navigate = useNavigate();
+  const params = useParams();
+  const [todos, setTodos] = useState([]);
+  const param = params.userId;
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => {
+        setTodos(json);
       });
   }, []);
 
+  const newTodos = useMemo(() => {
+    if (!param) return todos;
+    return todos.filter((element) => element.userId === Number(param));
+  }, [todos, param]);
+
   return (
     <>
-      <Search todos={todos} setCurrentTodos={setCurrentTodos}></Search>
-      <div className='grid'>
-        <div className='completed'>COMPLETED</div>
-        <div className='title'>TODO</div>
-        <div className='userId'>USERID</div>
-        
+      <Search
+        navigate={(userId) => {
+          if(!userId){
+            navigate('/');
+            return;
+          }
+          navigate(`/userId/${userId}`);
+        }}
+      ></Search>
+      <div className="grid">
+        <div className="completed">COMPLETED</div>
+        <div className="title">TODO</div>
+        <div className="userId">USERID</div>
       </div>
-      <TodoTable todos={currentTodos}></TodoTable>
+      <TodoTable todos={newTodos}></TodoTable>
     </>
   );
 }
--- src/routes/Search.jsx
+++ src/routes/Search.jsx
@@ -1,18 +1,11 @@
-function Search({ todos, setCurrentTodos }) {
+function Search({navigate}) {
+  
   return (
     <>
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          if (event.target.search.value === "") {
-            setCurrentTodos(todos);
-            return;
-          }
-          const userId = event.target.search.value;
-          const newTodos = todos.filter(
-            (element) => element.userId === Number(userId)
-          );
-          setCurrentTodos(newTodos);
+          navigate(event.target.search.value);
         }}
       >
         <input type="text" name="search" placeholder="Enter UserId"></input>
