dbf709a2777c340ee081953bab64a628054add02
SHIM-JINSEO <sjs011828@gmail.com>
2024-08-25 01:52:05 +0900

    feat: add text-decoration line-through

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,34 +4,39 @@ import { Link, useNavigate } from "react-router-dom";
 import "../App.css";
 import Search from "./Search.jsx";
 
-function Checkbox({ element }) {
-  const [checked, setChecked] = useState(element.completed);
 
-  return (
-    <input
-      type="checkbox"
-      checked={checked}
-      onChange={(event) => {
-        setChecked(!checked);
-      }}
-    />
-  );
-}
-
-function TodoTable({ todos }) {
+function TodoTable({ todos, checks, setChecks }) {
   return (
     <>
-      {todos.map((element) => (
-        <div className="grid" key={element.id}>
-          <div className="completed">
-            <Checkbox element={element} />
+      {todos.map((element) => {
+        return (
+          <div className="grid" key={element.id}>
+            <div className="completed">
+              <input
+                type="checkbox"
+                checked={checks[element.id - 1]}
+                onChange={(event) => {
+                  const newChecks = checks.map((check, index) =>
+                    index === element.id - 1 ? !check : check
+                  );
+                  setChecks(newChecks);
+                }}
+              />
+            </div>
+            <div
+              className="title"
+              style={
+                checks[element.id - 1]
+                  ? { textDecoration: "line-through" }
+                  : { textDecoration: "none" }
+              }
+            >
+              <Link to={`/${element.id}`}>{element.title}</Link>
+            </div>
+            <div className="userId">{element.userId}</div>
           </div>
-          <div className="title">
-            <Link to={`/${element.id}`}>{element.title}</Link>
-          </div>
-          <div className="userId">{element.userId}</div>
-        </div>
-      ))}
+        );
+      })}
     </>
   );
 }
@@ -40,12 +45,21 @@ function App() {
   const navigate = useNavigate();
   const params = useParams();
   const [todos, setTodos] = useState([]);
+  const [checks, setChecks] = useState([]);
   const param = params.userId;
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then((response) => response.json())
       .then((json) => {
-        setTodos(json);
+        const jsonOthers = [];
+        const jsonCompleted = [];
+        json.forEach(function (element) {
+          let { completed, ...rest } = element;
+          jsonOthers.push(rest);
+          jsonCompleted.push(completed);
+        });
+        setTodos(jsonOthers);
+        setChecks(jsonCompleted);
       });
   }, []);
 
@@ -58,8 +72,8 @@ function App() {
     <>
       <Search
         navigate={(userId) => {
-          if(!userId){
-            navigate('/');
+          if (!userId) {
+            navigate("/");
             return;
           }
           navigate(`/userId/${userId}`);
@@ -70,7 +84,11 @@ function App() {
         <div className="title">TODO</div>
         <div className="userId">USERID</div>
       </div>
-      <TodoTable todos={newTodos}></TodoTable>
+      <TodoTable
+        todos={newTodos}
+        checks={checks}
+        setChecks={setChecks}
+      ></TodoTable>
     </>
   );
 }
